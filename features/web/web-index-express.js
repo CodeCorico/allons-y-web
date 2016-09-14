65c6a40c6a3542d0d2a55ae95148418e75fdc143
Xavier Boubert <xavier@boubert.fr>
2016-09-14 11:44:43 +0200

    feat(web index express): let the possibility to expend the "web" object

--- features/web/web-index-express.js
+++ features/web/web-index-express.js
@@ -3,11 +3,12 @@
 module.exports = function($allonsy, $server, $BodyDataService) {
   var path = require('path'),
       fs = require('fs'),
-      indexFile = path.resolve('public/web/web-index.html');
+      indexFile = path.resolve('public/web/web-index.html'),
+      web = $BodyDataService.data('web') || {};
 
-  $BodyDataService.data('web', {
-    brand: process.env.WEB_BRAND || ''
-  });
+  web.brand = web.brand || process.env.WEB_BRAND || '';
+
+  $BodyDataService.data('web', web);
 
   $server.use(function(req, res) {
     res.send($BodyDataService.inject(fs.existsSync(indexFile) ? fs.readFileSync(indexFile, 'utf-8') : ''));
