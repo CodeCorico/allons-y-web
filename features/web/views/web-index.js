81e4adabef6692cfd5c2a6e0e0cf1dc49a59a3ff
Xavier Boubert <xavier@boubert.fr>
2016-09-19 19:38:58 +0200

    feat(web index): add fullscreen shortcut (F10)

--- features/web/views/web-index.js
+++ features/web/views/web-index.js
@@ -1,7 +1,11 @@
 (function() {
   'use strict';
 
-  window.bootstrap(['$Page', '$done', function($Page, $done) {
+  window.bootstrap([
+    '$Page', '$ShortcutsService', '$i18nService', '$done',
+  function($Page, $ShortcutsService, $i18nService, $done) {
+
+    var _fullscreenOpened = [false, false];
 
     ['left', 'right'].forEach(function(orientation, after) {
       $Page[orientation + 'ButtonAdd'] = function(name, button) {
@@ -49,6 +53,45 @@
         }
       };
     });
+
+    $ShortcutsService.register(
+      null,
+      'web-f10',
+      'F10',
+      $i18nService._('Close the context panels'),
+      function(e) {
+        // F10
+        var isShortcut = e.keyCode == 121 && !e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey;
+
+        if (isShortcut) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+
+        return isShortcut;
+      },
+      function() {
+        var $Layout = DependencyInjection.injector.view.get('$Layout'),
+            fullscreenOpened = [$Layout.leftContext().get('opened'), $Layout.rightContext().get('opened')];
+
+        if (!fullscreenOpened[0] && !fullscreenOpened[1]) {
+          if (_fullscreenOpened[0]) {
+            $Layout.leftContext().open(null, false);
+          }
+          if (_fullscreenOpened[1]) {
+            $Layout.rightContext().open(null, false);
+          }
+
+          _fullscreenOpened = [false, false];
+        }
+        else {
+          _fullscreenOpened = fullscreenOpened;
+
+          $Layout.leftContext().close(null, false);
+          $Layout.rightContext().close(null, false);
+        }
+      }
+    );
 
     $done();
   }]);
