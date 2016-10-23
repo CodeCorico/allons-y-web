238f659048931f4be37d9ea95c544970690e0e5a
Xavier Boubert <xavier@boubert.fr>
2016-10-24 01:03:29 +0200

    fix(web index express): use new $BodyDataService.data() with the req argument

--- features/web/web-index-express.js
+++ features/web/web-index-express.js
@@ -4,13 +4,13 @@ module.exports = function($allonsy, $server, $BodyDataService) {
   var path = require('path'),
       fs = require('fs'),
       indexFile = path.resolve('public/web/web-index.html'),
-      web = $BodyDataService.data('web') || {};
+      web = $BodyDataService.data(null, 'web') || {};
 
   web.brand = web.brand || process.env.WEB_BRAND || '';
 
-  $BodyDataService.data('web', web);
+  $BodyDataService.data(null, 'web', web);
 
   $server.use(function(req, res) {
-    res.send($BodyDataService.inject(fs.existsSync(indexFile) ? fs.readFileSync(indexFile, 'utf-8') : ''));
+    res.send($BodyDataService.inject(req, fs.existsSync(indexFile) ? fs.readFileSync(indexFile, 'utf-8') : ''));
   });
 };
