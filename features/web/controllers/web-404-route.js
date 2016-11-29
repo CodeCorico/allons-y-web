8407bae20bb42b969cb97c6d981c497f3f539f7d
Xavier Boubert <xavier@boubert.fr>
2016-11-29 17:22:10 +0100

    feat(web 404): add a special meta tag on prerender

--- features/web/controllers/web-404-route.js
+++ features/web/controllers/web-404-route.js
@@ -5,11 +5,21 @@ module.exports = [{
   priority: 'min',
 
   enter: [
-    '$WebService', '$Page', '$i18nService', '$context', '$Layout',
-  function($WebService, $Page, $i18nService, $context, $Layout) {
+    '$BodyDataService', '$WebService', '$Page', '$i18nService', '$context', '$Layout',
+  function($BodyDataService, $WebService, $Page, $i18nService, $context, $Layout) {
+    var prerender = $BodyDataService.data('prerender');
+
     document.title = $i18nService._('Page not found') + ' - ' + $Page.get('web').brand;
 
     $context.state.in404 = true;
+
+    if (prerender) {
+      var meta = document.createElement('meta');
+
+      meta.name = 'prerender-status-code';
+      meta.content = '404';
+      document.getElementsByTagName('head')[0].appendChild(meta);
+    }
 
     setTimeout(function() {
       $Layout.require('web-404')
@@ -19,8 +29,14 @@ module.exports = [{
     });
   }],
 
-  exit: ['$context', '$next', function($context, $next) {
+  exit: ['$BodyDataService', '$context', '$next', function($BodyDataService, $context, $next) {
     if ($context.state && $context.state.in404) {
+      var prerender = $BodyDataService.data('prerender');
+
+      if (prerender) {
+        $('meta[name="prerender-status-code"]').remove();
+      }
+
       $context.state.in404 = false;
 
       return DependencyInjection.injector.view.get('$WebService').teardown(null, $next);
