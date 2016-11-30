326de679607138f509f0e14f9ac1e9a022596b35
Xavier Boubert <xavier@boubert.fr>
2016-11-30 12:43:26 +0100

    feat(web service): add the updateUrl() method
    Used each time a page content is changed

--- features/web/models/web-service.js
+++ features/web/models/web-service.js
@@ -7,8 +7,16 @@ module.exports = function() {
 
       $AbstractService.call(this);
 
+      var _this = this;
+
       this.validateEmail = function(email) {
         return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(email);
+      };
+
+      this.updateUrl = function(url) {
+        _this.fire('updateUrl', {
+          url: url
+        });
       };
 
     })();
